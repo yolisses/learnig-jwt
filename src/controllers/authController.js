220deb8fd9ed3375f76c7e60a0faf8014e2e0a8a
Ulisses <m.stunik@gmail.com>
2021-05-29 10:56:25 -0300

    stop sending password on register

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -10,6 +10,7 @@ router.post('/register', async (req, res) => {
 	}
 	try {
 		const user = await User.create(req.body);
+		user.password = undefined;
 		return res.send({ user });
 	} catch {
 		return res.status(400).send({ error: 'Registration failed' });
