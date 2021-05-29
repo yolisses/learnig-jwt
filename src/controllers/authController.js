485c68f2056c10d36a960f0677447503905175be
Ulisses <m.stunik@gmail.com>
2021-05-29 10:54:29 -0300

    create a user already exist message

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,6 +4,10 @@ import User from '../models/user.js';
 const router = express.Router();
 
 router.post('/register', async (req, res) => {
+	const email = req.body.email;
+	if (await User.findOne({ email })) {
+		return res.status(400).send({ error: 'User already exists' });
+	}
 	try {
 		const user = await User.create(req.body);
 		return res.send({ user });
