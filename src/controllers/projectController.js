cb71aa37294eed521344ca0f005a001d6e81e7e2
Ulisses <m.stunik@gmail.com>
2021-05-30 12:04:18 -0300

    add authentication middleware

--- src/controllers/projectController.js
+++ src/controllers/projectController.js
@@ -1,6 +1,9 @@
 import express from 'express';
+import authMiddleware from '../middlewares/auth.js';
 
 const router = express.Router();
+
+router.use(authMiddleware);
 
 router.get('/', (req, res) => {
 	res.send({ ok: true });
---
+++ src/middlewares/auth.js
@@ -0,0 +1,25 @@
+import jwt from 'jsonwebtoken';
+import authConfig from '../config/auth.js';
+
+export default (req, res, next) => {
+	const authHeader = req.headers.authorization;
+
+	if (!authHeader) return res.status(401).send({ error: 'No token provided' });
+
+	const parts = authHeader.split(' ');
+
+	if (!parts.length === 2)
+		return res.status(401).send({ error: 'Token error' });
+
+	const [scheme, token] = parts;
+
+	if (!/^Bearer$/i.test(scheme))
+		return res.status(401).send({ error: 'Malformatted token' });
+
+	jwt.verify(token, authConfig.secret, (err, decoded) => {
+		if (err) return res.status(401).send({ error: 'Invalid token' });
+
+		req.userId = decoded.id;
+		return next();
+	});
+};
