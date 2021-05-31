74bb6e7202e7a298f646933ad75f5fe1e83c3d2d
Ulisses <m.stunik@gmail.com>
2021-05-31 09:49:08 -0300

    configure nodemailer-express-handlebars

--- src/app/modules/mailer.js
+++ src/app/modules/mailer.js
@@ -1,6 +1,18 @@
 import nodemailer from 'nodemailer';
+import hbs from 'nodemailer-express-handlebars';
+import path from 'path';
+
 import mailerConfig from '../../config/mailer.js';
 
 const transport = nodemailer.createTransport(mailerConfig);
+
+transport.use(
+	'compile',
+	hbs({
+		viewEngine: 'handlebars',
+		viewPath: path.resolve('./src/resources/mail/'),
+		extName: '.html',
+	})
+);
 
 export default transport;
