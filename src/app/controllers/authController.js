44a4e75e2a1ec46a16b6e353b8326b7419cff0c0
Ulisses <m.stunik@gmail.com>
2021-06-01 20:06:10 -0300

    add forgot password route

--- src/app/controllers/authController.js
+++ src/app/controllers/authController.js
@@ -2,6 +2,7 @@ import express from 'express';
 import User from '../models/user.js';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import crypto from 'crypto';
 
 import authConfig from '../../config/auth.js';
 
@@ -47,6 +48,23 @@ router.post('/authenticate', async (req, res) => {
 		user,
 		token: generateToken({ id: user.id }),
 	});
+});
+
+router.post('/forgot_password', async (req, res) => {
+	const { email } = req.body;
+	try {
+		const user = await User.findOne({ email });
+
+		if (!user) return res.status(400).send({ error: 'User not found' });
+
+		const token = crypto.randomBytes(20).toString('hex');
+
+		const expiresIn = new Date();
+		expiresIn.setHours(expiresIn.getHours() + 1);
+		
+	} catch {
+		res.status(400).send({ error: 'Error on forgot password, try again' });
+	}
 });
 
 export default (app) => app.use('/auth', router);
